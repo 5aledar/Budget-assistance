1c4a4b0acf73f510545e0cc37c369d4232f80b1a
khaledabdalslam99 <khaledabdalslam99@gmail.com>
2024-06-26 11:28:12 -0400

    bankController updated

--- backend/models/BankAccount.js
+++ backend/models/BankAccount.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const bankAccountSchema = new mongoose.Schema({
   userId: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  bankName: { type: String, required: true },
+  bankName: { type: String, required: true , unique: true},
   accountNumber: { type: String, required: true },
   balance: { type: Number, default: 0 }
 });
